b32d2dbb1402701850808c01e4ffa8d0de43117f
Jongwoo Choi <muvnelik@naver.com>
2024-01-14 15:37:16 +0900

    [feature] h2 console 접속 관련 권한 추가
    h2 콘솔을 확인할 수 있게 되었다.
    test.http 로는 요청이 제대로 날아가는데 테스트코드는 왜 안될까?

--- src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -4,12 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
@@ -18,14 +24,26 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests((authorizationManagerRequestMatcherRegistry ->
-                authorizationManagerRequestMatcherRegistry
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/api/users/**").permitAll()
-                        .anyRequest().authenticated()
-        ));
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-        return httpSecurity.build();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF비활성화??
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests((authorizeHttpRequest ->
+                authorizeHttpRequest
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+        );
+        //h2 console 열기 위해
+        http.headers()
+                .frameOptions().disable();
+
+        return http.build();
     }
 }
