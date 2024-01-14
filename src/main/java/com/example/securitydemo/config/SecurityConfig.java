134301c7d63313318ed4471ed7a92985a69a4c4c
Jongwoo Choi <muvnelik@naver.com>
2024-01-14 14:27:10 +0900

    [feature] SecurityConfig 추가 인증과 관련된 설정 추가

--- src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -1,14 +1,31 @@
 package com.example.securitydemo.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/api/users/**").permitAll()
+                        .anyRequest().authenticated()
+        ));
+
+        return httpSecurity.build();
     }
 }
