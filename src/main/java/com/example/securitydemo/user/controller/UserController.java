2013e251bd7deecb76b3c93c4a83765239354823
Jongwoo Choi <muvnelik@naver.com>
2024-01-16 23:30:57 +0900

    [fix] view 파일이 생성되었으므로 컨트롤러의 수정이 필요했음 +login시 입력받는 값을 email로.. 여기는 아직 수정이 필요할 듯 (진행중)

--- src/main/java/com/example/securitydemo/user/controller/UserController.java
+++ src/main/java/com/example/securitydemo/user/controller/UserController.java
@@ -2,24 +2,38 @@ package com.example.securitydemo.user.controller;
 
 import com.example.securitydemo.user.dto.RegisterRequestDto;
 import com.example.securitydemo.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+//@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequestDto requestDto) {
+    // 회원가입
+//    @PostMapping("/register")
+//    public ResponseEntity<String> register(@RequestBody RegisterRequestDto requestDto) {
+//        userService.register(requestDto);
+//        return ResponseEntity.status(HttpStatus.CREATED).body("회원 가입 성공");
+//    }
+
+    @PostMapping(value = "/register")
+    public String register(RegisterRequestDto requestDto) {
         userService.register(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body("회원 가입 성공");
+        return "redirect:/login"; //회원가입 후 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
--- src/main/java/com/example/securitydemo/user/dto/LoginRequestDto.java
+++ src/main/java/com/example/securitydemo/user/dto/LoginRequestDto.java
@@ -9,12 +9,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequestDto {
-    private String username;
+
+    private String email;
     private String password;
 
     public User toEntity() {
         return User.builder()
-                .username(username)
+                .email(email)
                 .password(password)
                 .build();
     }
