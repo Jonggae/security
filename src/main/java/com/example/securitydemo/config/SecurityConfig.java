3ea5745d66fd2c83ff1bce8ad28c06421997e602
Jongwoo Choi <muvnelik@naver.com>
2024-01-16 23:28:39 +0900

    [feature] 폼 로그인을 위한 secuityConfig 수정

--- src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -1,14 +1,15 @@
 package com.example.securitydemo.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,32 +18,42 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    //인증관리자
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // CSRF비활성화??
-        http.csrf(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests((authorize) ->
+                        authorize
+                                .requestMatchers("/api/users/**").permitAll() // API 접근 허용 설정
+                                .requestMatchers("/h2-console/**").permitAll() // H2 console 접근 허용 설정
+                                .requestMatchers("/login").permitAll()
+                                .requestMatchers("/register").permitAll()
+                                .requestMatchers("/home").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home").permitAll()
 
-        http.authorizeHttpRequests((authorizeHttpRequest ->
-                authorizeHttpRequest
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/api/users/**").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll() //h2-console 사용
-                        .anyRequest().authenticated())
-        );
-        //h2 console 열기 위해
-        http.headers()
-                .frameOptions().disable();
+                )
 
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // H2 console을 위한 설정
+                .csrf(AbstractHttpConfigurer::disable); // CSRF 비활성화
         return http.build();
     }
 }
