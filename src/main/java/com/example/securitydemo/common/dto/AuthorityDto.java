89d266fb3ee06b9473884273f8562ae208c402a3
Jongwoo Choi <muvnelik@naver.com>
2024-01-24 14:29:40 +0900

    [feature] 권한 (admin, user)과 관련된 Dto. 아직 크게 사용하지 않음.

---
+++ src/main/java/com/example/securitydemo/common/dto/AuthorityDto.java
@@ -0,0 +1,12 @@
+package com.example.securitydemo.common.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+   private String authorityName;
+}
